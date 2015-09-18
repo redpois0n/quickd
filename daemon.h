68af7b6d43c9de530134721c934b3f6b76b11aaa
redpois0n <red@cmail.nu>
2015-09-18 10:32:35 +0200

    First commit

---
+++ daemon.h
@@ -0,0 +1,40 @@
+#ifndef DAEMON_H
+#define DAEMON_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+
+#define EXIT_SUCCESS 0
+
+void deploy_daemon() {
+	pid_t pid, sid;
+
+	pid = fork();
+	if (pid < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	if (pid > 0) {
+		exit(EXIT_SUCCESS);
+	}
+
+	umask(0);
+
+	sid = setsid();
+	if (sid < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	if (chdir("/") != 0) {
+		exit(EXIT_FAILURE);
+	}
+}
+
+#endif
